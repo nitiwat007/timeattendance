31d45220f248153a04ec19b4456b17182e031281
Nitiwat Thongkao <nitiwat007@gmail.com>
2018-02-15 13:44:14 +0700

    add loading to main page use ActivityIndicator

--- components/Main.js
+++ components/Main.js
@@ -1,6 +1,6 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator, Modal } from 'react-native';
 import { Container, Drawer, Content } from 'native-base'
 import { connect } from 'react-redux';
 
@@ -16,7 +16,8 @@ class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            events: []
+            events: [],
+            isLoading: true
         }
     }
 
@@ -29,24 +30,29 @@ class Main extends Component {
     };
 
     componentDidMount() {
+        this.setState({
+            isLoading: true
+        })
         const memberID = this.props.userDetail[0]
 
         EventApi.getByMemberID(memberID).then(data => {
             this.setState({
-                events: data
-            })       
+                events: data,
+                isLoading: false
+            })
         })
     }
 
     render() {
 
-        const { events } = this.state
+        const { events, isLoading } = this.state
 
         return (
             <Drawer ref={(ref) => { this.drawer = ref }} content={<SideBar />} onClose={() => this.closeDrawer()}>
                 <Container>
                     <AppHeader title='หน้าหลัก' openDrawer={this.openDrawer.bind(this)} />
                     <Content>
+                        {isLoading && (<ActivityIndicator style={styles.ActivityIndicator} size='large' color='#5DADE2' />)}
                         {events.map((event, i) =>
                             <EventBox key={i} title={event.EventNameEN} date={event.EventDate} imgUri={event.EventBannerLink} eventID={event.EventID} />
                         )}
@@ -63,6 +69,9 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#E6E6E6'
     },
+    ActivityIndicator: {
+        paddingTop: 20
+    }
 });
 
 function mapStateToProps(state) {
